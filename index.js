9ca3829a5a1e5f4f1d3e9152154628eb9a6b7ebd
Kenyi Hancco <kenyihq@gmail.com>
2022-05-27 21:55:10 -0500

    Show rows from database

--- index.js
+++ index.js
@@ -13,13 +13,27 @@ var app = new function() {
         .then(r=>r.json())
         .then(respuesta=>{
             console.log(respuesta);
+
+            respuesta.map(
+                function (employee, index, array) {
+                    data += `<tr>
+                                <td>${employee.id}</td>
+                                <td>${employee.nombre}</td>
+                                <td>${employee.correo}</td>
+                                <td>
+                                    <button class="btn btn-danger" onclick="app.delete(${employee.id})">Eliminar</button>
+                                </td>
+                            </tr>`;
+                }
+            );
+
+            return this.employee.innerHTML = data;
         })
         .catch(error=>{
             console.log(error);
         })
 
-        data = "<tr><th>1</th><td>Oscar</td><td>oscar@gmail.com</td><td>Editar | Borrar</td></tr>";
-        return this.employee.innerHTML = data;
+        //data = "<tr><th>1</th><td>Oscar</td><td>oscar@gmail.com</td><td>Editar | Borrar</td></tr>";
     };
 
     this.add = function() {
